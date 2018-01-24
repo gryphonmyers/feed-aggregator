ec84d152e616c7dddaa715c60e39337253bc7895
Gryphon Myers <gryphon.myers@2k.com>
2018-01-24 09:38:04 -0800

    change mixwith dep

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var mix = require('mixwith').mix;
+var mix = require('mixwith-es5').mix;
 var EventEmitterMixin = require('event-emitter-mixin');
 var uniq = require('array-uniq');
 var compact = require('array-compact');
